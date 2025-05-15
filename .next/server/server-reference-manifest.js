6f29254a6afe4c85a69f4b74cc459de8a3b4ef93
Jean Paul <ark2784@gmail.com>
2025-05-14 19:09:13 -0500

    lets rock

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Bqz27eo9l2jLxXdNIb+UEKYaZ3RmlITRAiDZHEhRS8s=\"\n}"
---
+++ data/index.ts
@@ -0,0 +1,189 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a N8N for Automated Workflow Systems",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "AI Telegram Agent",
+    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    img: "/pp1.svg",
+    iconLists: ["/re.svg", "/tele.svg", "/ts.svg", "/next.svg", "/supa.svg"],
+    link: "https://github.com/alainjpdev/agenteJean",
+  },
+  {
+    id: 2,
+    title: "MiCasApp - Real Estate App",
+    des: "MicasApp is a real estate platform designed to help users discover, manage, and connect with property listings in a user-friendly and efficient way.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/ts.svg", "/supa.svg"],
+    link: "https://micasapp.vercel.app",
+  },
+  {
+    id: 3,
+    title: "Moke Tulum - Design & Own Your Dream Moke",
+    des: "Moke Tulum is a web app that lets users custom-build and purchase their own Moke vehicle with a unique, Tulum-inspired vibe..",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/supa.svg"],
+    link: "https://moketulum.com",
+  },
+  
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Jean Paul was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Jean Paul's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Jean Paul is the ideal partner.",
+    name: "Mark Desrochers",
+    title: "Director of Desrochers Real Estate",
+  },
+  {
+    quote:
+      "Jean Paul was my former manager at Waves Property Management and played a key role in the early development of MicasApp. His experience in property operations and guest services helped shape the vision and functionality of the platform. ",
+    name: "Angelo Gil",
+    title: "Director of Waves Property Management",
+    img:"/appName.svg"
+  },
+
+];
+
+export const companies = [
+  
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - JSM Tech",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+{/*export const socialMedia = [
+  /*{
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];*/}
